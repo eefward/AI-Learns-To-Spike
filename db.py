9a74afffcf9b88297edd44303479a0b6fe0dc720
GuestyTheBesty <guestythebesty841@gmail.com>
2025-01-18 22:40:16 -0500

    ....

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import ast
 con = sqlite3.connect("moves.db")
 cur = con.cursor()
 
-def create_db(start, end):
+def create_db(amount):
     con = sqlite3.connect("moves.db")
     cur = con.cursor()
 
@@ -19,13 +19,12 @@ def create_db(start, end):
          'rm': 0.125, 
          'rb': 0.125, 
          'mt': 0.125, 
-         'mm': 0.125, 
          'mb': 0.125, 
          'lt': 0.125, 
          'lm': 0.125, 
          'lb': 0.125}
     
-    for i in range(start, end):
+    for i in range(1, amount + 1):
         cur.execute("INSERT INTO intervals (interval, dict) VALUES (?, ?)", (i, str(a)))
 
     con.commit()
@@ -57,4 +56,5 @@ def wipe_db():
     con.commit()
     con.close()
 
+create_db(24)
 main(.25, 6)
